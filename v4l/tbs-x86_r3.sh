26bb2cb6c4ed061786013a47c9bc003ae5b3e661
Tycho Lursen <tycholursen@gmail.com>
2014-06-02 15:43:12 +0200

    Add suport for 6281 and 6285 (in DVB-C mode)

--- v4l/tbs-x86_r3.sh
+++ v4l/tbs-x86_r3.sh
@@ -4,4 +4,6 @@ cp ./v4l/tbsctrl.o.x86_r3 ./v4l/tbsctrl.o
 
 cp ./v4l/tbs6680fe_driver.o.x86_r3 ./v4l/tbs6680fe_driver.o
 
+cp ./v4l/tbs62x1fe_driver.o.x86_r3 ./v4l/tbs62x1fe_driver.o
+
 echo "TBS drivers configured for x86_r3 platform."
